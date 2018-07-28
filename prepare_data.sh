6fc4e676702b9023edabcf4977fa07b2383bc6c9
xiaowohmbb <hilici@foxmail.com>
2018-07-28 14:12:58 +0800

    zhushi

--- prepare_data.sh
+++ prepare_data.sh
@@ -7,11 +7,11 @@ mysqlDatabase=test
 
 mysqlCmd="mysql -h${mysqlHost} -P${mysqlPort} -D${mysqlDatabase} -u${mysqlUser} -p${mysqlPasswd}"
 
-$mysqlCmd -e "CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */";
-$mysqlCmd -e "CREATE DATABASE IF NOT EXISTS `test1` /*!40100 DEFAULT CHARACTER SET utf8 */";
-$mysqlCmd -e "CREATE DATABASE IF NOT EXISTS `test2` /*!40100 DEFAULT CHARACTER SET utf8 */";
+$mysqlCmd -e "CREATE DATABASE IF NOT EXISTS test";
+$mysqlCmd -e "CREATE DATABASE IF NOT EXISTS test1";
+$mysqlCmd -e "CREATE DATABASE IF NOT EXISTS test2";
 $mysqlCmd -e "create table IF NOT EXISTS test.t_name(id int primary key,name varchar(250));";
 $mysqlCmd -e "create table IF NOT EXISTS test1.t_address(id int primary key,address varchar(250));";
 $mysqlCmd -e "create table IF NOT EXISTS test2.t_name_address(id int primary key,name varchar(250),address varchar(250));";
-$mysqlCmd -e "insert into test.t_name values (1,'a')";
-$mysqlCmd -e "insert into test1.t_address values (1,'b')";
+$mysqlCmd -e "insert ignore into test.t_name values (1,'a')";
+$mysqlCmd -e "insert ignore into test1.t_address values (1,'b')";
